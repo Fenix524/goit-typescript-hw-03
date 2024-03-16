87e120e10ba3ec5b255f97745fa2c66b3f52585c
Herzanich Georgii <109143250+Fenix524@users.noreply.github.com>
2024-03-16 20:34:36 +0100

    ready all tasks

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,62 @@
-const key = new Key();
+class Key {
+	private signature: string
 
-const house = new MyHouse(key);
-const person = new Person(key);
+	constructor() {
+		this.signature = Math.random().toString(36).substring(2)
+	}
+	getSignature(): string {
+		return this.signature
+	}
+}
+class Person {
+	constructor(private key: Key) {}
+	getKey(): Key {
+		return this.key
+	}
+}
 
-house.openDoor(person.getKey());
+abstract class House {
+	protected door: boolean
+	protected key: Key
+	protected tenants: Person[] = []
 
-house.comeIn(person);
+	constructor(key: Key) {
+		this.key = key
+		this.door = false
+	}
 
+	comeIn(person: Person): void {
+		if (this.door) {
+			this.tenants.push(person)
+		}
+	}
 
-export {};
+	abstract openDoor(key: Key): void
+}
+
+class MyHouse extends House {
+	constructor(key: Key) {
+		super(key)
+	}
+
+	openDoor(key: Key): void {
+		if (key.getSignature() === this.key.getSignature()) {
+			this.door = true
+			console.log('The door is opened.')
+		} else {
+			console.log('The key does not match.')
+		}
+	}
+}
+
+const key = new Key()
+console.log(key.getSignature())
+
+const house = new MyHouse(key)
+const person = new Person(key)
+
+house.openDoor(person.getKey())
+
+house.comeIn(person)
+
+// export {}
